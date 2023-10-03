513277c1572b9d78317bbf367aa273220cd4f11d
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-10-03 16:53:18 +0700

    Update counting for the last column poitn

--- test.cpp
+++ test.cpp
@@ -24,30 +24,41 @@ For each test case, output a single integer — the total number of points of th
 int find_total_point(char tmp[10][10]) {
     int sum = 0;
     int point_flag = 0;
-    int outer_last_col = 10;
+    int outer_last_col = 9;
     int outer_first_col = 0;
     
     // row = i; col = j
     for(int i = 0; i < 10;i++) {
         point_flag++;
-        for(int j = outer_first_col; j < outer_last_col; j++) { // In the position j - 1, the point is always point - 1
+        for(int j = outer_first_col; j <= outer_last_col; j++) { // In the position j - 1, the point is always point - 1
             // Calculate the previous column before
             if(j == outer_first_col) {
                 int n = outer_first_col - 1;
                 int counter_flag = 0;
-                for(counter_flag; counter_flag == n; counter_flag++) {
+                for(counter_flag; counter_flag <= n; counter_flag++) {
                     if(tmp[i][counter_flag] == 'X') {
                         sum = sum + (counter_flag + 1);
                     }
                 }
             }
-            
+            else if(j == outer_last_col) { // Counting for the last column of the point zone
+                int n = outer_last_col + 1;
+                int counter_flag = 0;
+                for(counter_flag; counter_flag <= 10 - n; counter_flag++) {
+                    if(tmp[i][counter_flag] == 'X') {
+                        sum = sum + (counter_flag + 1);
+                    }
+                }
+            }
+            // For narrow in the right current point zone
             if(tmp[i][j] == 'X') {
                 sum += point_flag;
             }
             else {
                 continue;
             }
+
+            // For reverse from 5 till 10, we change the order of the point zone follow the alogrithm below:
         }
         outer_first_col++;
         outer_last_col--;
@@ -57,24 +68,17 @@ int find_total_point(char tmp[10][10]) {
 
 void solution() {
     int test_case;
-    char table[10][10];
-
     // Input number of test case
     std::cin >> test_case;
     //Input the table base on test case
     for(int i = 0; i < test_case;i++) {
+        char table[10][10];
         for(int r = 0; r < 10; r++) {
             for(int c = 0; c < 10; c++) {
                 std::cin >> table[r][c];
             }
         }
-        // Test for the input
-        // for(int j = 0; j < 10; j++) {
-        //     for(int k = 0; k < 10; k ++) {
-        //         std::cout << table[j][k] << " ";
-        //     }
-        //     std:: cout << "\n";
-        // }
+        std::cout << find_total_point(table);
     }
     
     
